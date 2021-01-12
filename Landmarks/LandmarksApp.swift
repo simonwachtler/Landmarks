d3d3394c4beb8bd3c4f4b02a258cd484c1398b00
Simon Wachtler <simon.wachtler@gmail.com>
2021-01-12 22:07:31 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Gernot Wachtler on 12/01/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
