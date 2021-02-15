fe581b5a7919d1fd7bded73d40708b57fb4dc9d1
simonwachtler <simon.wachtler@gmail.com>
2021-02-15 18:55:46 +0100

    Composing Complex Interfaces (1-3):  Category View, Category List, Category Row

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Simon Wachtler on 15/02/21.
+//
+
+import SwiftUI
+
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+
+    var body: some View {
+        
+        NavigationView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
+ 
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
