c5bfc2aa66155b86bf7747f567e83c6bc29d13a6
simonwachtler <simon.wachtler@gmail.com>
2021-02-25 16:49:26 +0100

    Creating a macOS App (5): Add a Built-in Menu Command

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,15 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Simon Wachtler on 25/02/21.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+        
+}
+}
