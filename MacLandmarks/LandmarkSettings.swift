294be28a8fa4b5decf5cf13654f5e37f9c7b8f25
simonwachtler <simon.wachtler@gmail.com>
2021-02-25 21:12:46 +0100

    Creating a macOS App (7): Add Preferences with a Settings Scene

---
+++ MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by Simon Wachtler on 25/02/21.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    @AppStorage("MapView.zoom")
+    private var zoom: MapView.Zoom = .medium
+    
+    var body: some View {
+        Form {
+        Picker("Map Zoom:", selection: $zoom) {
+            ForEach(MapView.Zoom.allCases) { level in
+                Text(level.rawValue)
+            }
+        }
+        .pickerStyle(InlinePickerStyle())
+    }
+    .frame(width: 300)
+    .navigationTitle("Landmark Settings")
+    .padding(80)
+    
+}
+}
+
+struct LandmarkSettings_Preview: PreviewProvider {
+    static var previews: some View {
+        LandmarkSettings()
+    }
+}
